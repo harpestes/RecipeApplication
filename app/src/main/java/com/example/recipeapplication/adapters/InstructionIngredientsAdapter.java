00495a9bf1161293ce3c578db884675d6cb069b9
Dykalo Andrij <dikaloandrij@gmail.com>
2024-05-11 19:53:46 +0300

    refactor: Change theme and application name

--- app/src/main/java/com/example/recipeapplication/adapters/InstructionIngredientsAdapter.java
+++ app/src/main/java/com/example/recipeapplication/adapters/InstructionIngredientsAdapter.java
@@ -3,7 +3,6 @@ package com.example.recipeapplication.adapters;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
