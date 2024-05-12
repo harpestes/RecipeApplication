736eeb7ad337e2507bfbe7687073b7a614ed0052
Dykalo Andrij <dikaloandrij@gmail.com>
2024-05-12 14:28:27 +0300

    fix: Make ImageView use placeholder if path to image is empty

--- app/src/main/java/com/example/recipeapplication/adapters/InstructionIngredientsAdapter.java
+++ app/src/main/java/com/example/recipeapplication/adapters/InstructionIngredientsAdapter.java
@@ -3,6 +3,7 @@ package com.example.recipeapplication.adapters;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -32,7 +33,13 @@ public class InstructionIngredientsAdapter extends RecyclerView.Adapter<Instruct
     public void onBindViewHolder(@NonNull InstructionIngredientViewHolder holder, int position) {
         holder.getTextView_instructions_step_item().setText(list.get(position).getName());
         holder.getTextView_instructions_step_item().setSelected(true);
-        Picasso.get().load(list.get(position).getImage()).into(holder.getImageView_instructions_step_item());
+        String imagePath = list.get(position).getImage();
+        if (imagePath.trim().isEmpty()) {
+            holder.getImageView_instructions_step_item().setImageResource(R.drawable.image_placeholder);
+            holder.getImageView_instructions_step_item().setScaleType(ImageView.ScaleType.CENTER_CROP);
+        } else {
+            Picasso.get().load(imagePath).into(holder.getImageView_instructions_step_item());
+        }
     }
 
     @Override
